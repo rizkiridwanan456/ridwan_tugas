1b0fab26cd25e1441fde3a119980220e0dba9bb0
Rizki Ridwan <119581876+rizkiridwanan456@users.noreply.github.com>
2022-12-01 17:26:31 +0700

    Add files via upload

---
+++ api/config.php
@@ -0,0 +1,7 @@
+<?php
+define ('DB_NAME','dbs_api');
+define ('DB_USER','root');
+define ('DB_PASSWORD','');
+define ('DB_HOST','localhost');
+    $kon = mysqli_connect("localhost", "root", "", "dbs_api") or die(mysqli_error());
+?>
